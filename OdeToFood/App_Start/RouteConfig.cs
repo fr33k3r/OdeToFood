24b38192795483ecee63290de8434b303387825d
fr33k3r <fr33k3r@yahoo.com>
2015-12-08 11:29:28 +0200

    End of Chapter 2 - Controllers in ASP.NET MVC 4

--- OdeToFood/App_Start/RouteConfig.cs
+++ OdeToFood/App_Start/RouteConfig.cs
@@ -13,6 +13,17 @@ namespace OdeToFood
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+            // /Cuisine/french
+
+            routes.MapRoute(
+                name: "Cuisine",
+                url: "cuisine/{name}",
+                defaults: new { controller = "Cuisine", action = "Search", name = UrlParameter.Optional }
+            );
+
+            // /Home/index
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
